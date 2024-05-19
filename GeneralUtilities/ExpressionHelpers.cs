3f8aa0ded6cb334ed96b7ca7277f1ad96bbbec1b
Mark Olbert <mark@arcabama.com>
2024-05-19 10:53:32 -0700

    create expression helpers

---
+++ GeneralUtilities/ExpressionHelpers.cs
@@ -0,0 +1,113 @@
+﻿#region copyright
+// Copyright (c) 2021, 2022, 2023 Mark A. Olbert 
+// https://www.JumpForJoySoftware.com
+// ExpressionExtensions.cs
+//
+// This file is part of JumpForJoy Software's MiscellaneousUtilities.
+// 
+// MiscellaneousUtilities is free software: you can redistribute it and/or modify it 
+// under the terms of the GNU General Public License as published by the 
+// Free Software Foundation, either version 3 of the License, or 
+// (at your option) any later version.
+// 
+// MiscellaneousUtilities is distributed in the hope that it will be useful, but 
+// WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
+// or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License 
+// for more details.
+// 
+// You should have received a copy of the GNU General Public License along 
+// with MiscellaneousUtilities. If not, see <https://www.gnu.org/licenses/>.
+#endregion
+
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using Microsoft.Extensions.Logging;
+
+namespace J4JSoftware.Utilities;
+
+public class ExpressionHelpers( ILoggerFactory? loggerFactory )
+{
+    private readonly ILogger? _logger = loggerFactory?.CreateLogger<ExpressionHelpers>();
+
+    public bool TryGetPropertyInfo<TRoot, TProp>(
+        Expression<Func<TRoot, TProp>> propExpr,
+        out PropertyInfo? propInfo,
+        [ CallerMemberName ] string caller = ""
+    )
+    {
+        propInfo = propExpr.Body is MemberExpression memExpr ? (PropertyInfo) memExpr.Member : null;
+
+        if( propInfo == null )
+            _logger?.InvalidExpression( propExpr.Body.NodeType, caller );
+
+        return propInfo != null;
+    }
+
+    public List<PropertyInfo> GetPropertyInfoPath<TRoot, TProp>(
+        Expression<Func<TRoot, TProp>> propExpr,
+        [ CallerMemberName ] string caller = ""
+    )
+    {
+        var retVal = new List<PropertyInfo>();
+
+        var curExpr = propExpr.Body;
+
+        while( curExpr != null )
+        {
+            switch( curExpr )
+            {
+                case MemberExpression memExpr:
+                    retVal.Add( (PropertyInfo) memExpr.Member );
+                    curExpr = memExpr.Expression;
+
+                    break;
+
+                default:
+                    curExpr = null;
+                    break;
+            }
+        }
+
+        if( retVal.Count == 0 )
+            _logger?.EmptyPropertyInfoPath( propExpr.ToString() );
+
+        // the list is leaf first (i.e., the root is the last element in the list)
+        return retVal;
+    }
+
+    // thanx to https://gist.github.com/jrgcubano/6e4df87913411ee9db0c68efc5fc41a3 for this 
+    public Action<TRoot, TProp>? CreatePropertySetter<TRoot, TProp>(
+        Expression<Func<TRoot, TProp>> property,
+        [ CallerMemberName ] string caller = ""
+    )
+    {
+        var propInfoPath = GetPropertyInfoPath( property );
+
+        // make the list root first
+        propInfoPath.Reverse();
+
+        if( propInfoPath.Count == 0 )
+            return null;
+
+        var rootArg = Expression.Parameter(typeof(TRoot), "root");
+        var newValueArg = Expression.Parameter(typeof(TProp), "newValue");
+
+        Expression? prevExpr = null;
+
+        foreach( var propInfo in propInfoPath )
+        {
+            // first/root element
+            var curExpr = propInfo == propInfoPath.First() ? rootArg : prevExpr;
+
+            prevExpr = Expression.Property( curExpr, propInfo );
+
+            if( propInfo == propInfoPath.Last() )
+                prevExpr = Expression.Assign( prevExpr, newValueArg );
+        }
+
+        var lambdaExpr = Expression.Lambda<Action<TRoot, TProp>>( prevExpr!, [rootArg, newValueArg] );
+
+        return lambdaExpr.Compile();
+    }
+}
---
+++ GeneralUtilities/InternalLogMessages.cs
@@ -0,0 +1,47 @@
+﻿using System.Linq.Expressions;
+using System.Runtime.CompilerServices;
+using Microsoft.Extensions.Logging;
+
+namespace J4JSoftware.Utilities;
+
+internal static partial class InternalLogMessages
+{
+    [LoggerMessage(LogLevel.Error, "{caller}: Invalid ExpressionType {exprType} (called by {parentCaller})")]
+    internal static partial void InvalidExpression(
+        this ILogger logger,
+        ExpressionType exprType,
+        string parentCaller,
+        [ CallerMemberName ] string caller = ""
+    );
+
+    [LoggerMessage(LogLevel.Error, "{caller}: Property {propName} does not have a set method (called by {parentCaller})")]
+    internal static partial void MissingSetMethod(
+        this ILogger logger,
+        string propName,
+        string parentCaller,
+        [CallerMemberName] string caller = ""
+    );
+
+    [LoggerMessage(LogLevel.Error, "{caller}: Empty PropertyInfo path collection for expression '{propExpr}'")]
+    internal static partial void EmptyPropertyInfoPath(
+        this ILogger logger,
+        string propExpr,
+        [ CallerMemberName ] string caller = ""
+    );
+
+    [LoggerMessage(LogLevel.Error, "{caller}: Property {propName} in expression '{propExpr}' has no declaring type")]
+    internal static partial void UndefinedDeclaringType(
+        this ILogger logger,
+        string propName,
+        string propExpr,
+        [ CallerMemberName ] string caller = ""
+    );
+
+    [LoggerMessage(LogLevel.Error, "{caller}: Property {propName} in expression '{propExpr}' has no set method")]
+    internal static partial void UndefinedSetMethod(
+        this ILogger logger,
+        string propName,
+        string propExpr,
+        [CallerMemberName] string caller = ""
+    );
+}
