203c67a053f7e7be6fe8765f7e58d504b8c93809
Mark Olbert <mark@arcabama.com>
2024-05-19 10:53:47 -0700

    migrate from MiscellaneousUtilities assembly

---
+++ GeneralUtilities/ExceptionExtensions.cs
@@ -0,0 +1,52 @@
+﻿#region copyright
+// Copyright (c) 2021, 2022, 2023 Mark A. Olbert 
+// https://www.JumpForJoySoftware.com
+// ExceptionExtensions.cs
+//
+// This file is part of JumpForJoy Software's MiscellaneousUtilities.
+// 
+// MiscellaneousUtilities is free software: you can redistribute it and/or modify it 
+// under the terms of the GNU General Public License as published by the 
+// Free Software Foundation, either version 3 of the License, or 
+// (at your option) any later version.
+// 
+// MiscellaneousUtilities is distributed in the hope that it will be useful, but 
+// WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
+// or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License 
+// for more details.
+// 
+// You should have received a copy of the GNU General Public License along 
+// with MiscellaneousUtilities. If not, see <https://www.gnu.org/licenses/>.
+#endregion
+
+using System;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace J4JSoftware.Utilities;
+
+public static class ExceptionExtensions
+{
+    public static string FormatException(
+        this Exception e,
+        string message,
+        [CallerMemberName] string callerName = "",
+        [CallerFilePath] string callerFilePath = "",
+        [CallerLineNumber] int callerLineNum = 0
+    )
+    {
+        var retVal = new StringBuilder(message);
+
+        retVal.Append($"\n\tException type:\t{e.GetType().Name}");
+        retVal.Append($"\n\tDetails:\t{e.Message}");
+
+        if (e.InnerException != null)
+            retVal.Append($"\n\tInner Details:\t{e.InnerException.Message}");
+
+        retVal.Append($"\n\n\tCalled by:\t{callerName}");
+        retVal.Append($"\n\tSource file:\t{callerFilePath}");
+        retVal.Append($"\n\tLine number:\t{callerLineNum}");
+
+        return retVal.ToString();
+    }
+}
