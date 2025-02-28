d269a409ffdd57a1d81d54823fc464024a5aa7b0
Mark Olbert <mark@arcabama.com>
2025-02-28 09:05:09 -0800

    merge files from miscellaneousutilities

---
+++ src/ExpressionExtensions.cs
@@ -0,0 +1,74 @@
+﻿#region copyright
+// Copyright (c) 2021, 2022, 2023 Mark A. Olbert 
+// https://www.JumpForJoySoftware.com
+// ExpressionExtensions.cs
+//
+// This file is part of JumpForJoy Software's MiscellaneousUtilities.
+// 
+// MiscellaneousUtilities is free software: you can redistribute it and/or modify it 
+// under the terms of the GNU General Public License as published by the 
+// Free Software Foundation, either version 3 of the License, or 
+// (at your option) any later version.
+// 
+// MiscellaneousUtilities is distributed in the hope that it will be useful, but 
+// WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
+// or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License 
+// for more details.
+// 
+// You should have received a copy of the GNU General Public License along 
+// with MiscellaneousUtilities. If not, see <https://www.gnu.org/licenses/>.
+#endregion
+
+using System.Linq.Expressions;
+using System.Reflection;
+using System;
+
+namespace J4JSoftware.Utilities;
+
+[Obsolete("Use J4JSoftware.Utilities.ExtensionHelpers instead")]
+public static class ExpressionExtensions
+{
+    public static PropertyInfo GetPropertyInfo<TContainer, TProp>(
+        this Expression<Func<TContainer, TProp>> expression
+    ) =>
+        expression.Body switch
+        {
+            null => throw new ArgumentNullException( nameof( expression ) ),
+            UnaryExpression { Operand: MemberExpression me } => (PropertyInfo) me.Member,
+            MemberExpression me => (PropertyInfo) me.Member,
+            _ => throw new ArgumentException( $"The expression isn't a valid property. [ {expression} ]" )
+        };
+
+    // thanx to https://gist.github.com/jrgcubano/6e4df87913411ee9db0c68efc5fc41a3
+    // for these next two methods
+    public static Action<TEntity, TProperty> CreateSetter<TEntity, TProperty>(Expression<Func<TEntity, TProperty>> property)
+    {
+        var propertyInfo = property.GetPropertyInfo();
+
+        var setMethod = propertyInfo.GetSetMethod()
+         ?? throw new ArgumentNullException( $"Property {propertyInfo.Name} does not have a public setter" );
+
+        var instance = Expression.Parameter(typeof(TEntity), "instance");
+        var parameter = Expression.Parameter(typeof(TProperty), "param");
+
+        var body = Expression.Call(instance, setMethod, parameter);
+        var parameters = new[] { instance, parameter };
+
+        return Expression.Lambda<Action<TEntity, TProperty>>(body, parameters).Compile();
+    }
+
+    public static Func<TEntity, TProperty> CreateGetter<TEntity, TProperty>(Expression<Func<TEntity, TProperty>> property)
+    {
+        var propertyInfo = property.GetPropertyInfo();
+
+        var getMethod = propertyInfo.GetGetMethod()
+         ?? throw new ArgumentNullException($"Property {propertyInfo.Name} does not have a public getter");
+
+        var instance = Expression.Parameter(typeof(TEntity), "instance");
+
+        var body = Expression.Call(instance, getMethod);
+        var parameters = new[] { instance };
+
+        return Expression.Lambda<Func<TEntity, TProperty>>(body, parameters).Compile();
+    }
+}
---
+++ src/StringExtensions.cs
@@ -0,0 +1,79 @@
+﻿#region copyright
+// Copyright (c) 2021, 2022, 2023 Mark A. Olbert 
+// https://www.JumpForJoySoftware.com
+// StringExtensions.cs
+//
+// This file is part of JumpForJoy Software's MiscellaneousUtilities.
+// 
+// MiscellaneousUtilities is free software: you can redistribute it and/or modify it 
+// under the terms of the GNU General Public License as published by the 
+// Free Software Foundation, either version 3 of the License, or 
+// (at your option) any later version.
+// 
+// MiscellaneousUtilities is distributed in the hope that it will be useful, but 
+// WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
+// or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License 
+// for more details.
+// 
+// You should have received a copy of the GNU General Public License along 
+// with MiscellaneousUtilities. If not, see <https://www.gnu.org/licenses/>.
+#endregion
+
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+
+// ReSharper disable ExplicitCallerInfoArgument
+
+namespace J4JSoftware.Utilities;
+
+public static class StringExtensions
+{
+    public record HashContext( bool CaseInsensitive = true, bool IgnoreWhiteSpace = true );
+
+    public static string CalculateHash( this HashContext context, params string?[] toHash ) =>
+        CalculateHash( context.CaseInsensitive, context.IgnoreWhiteSpace, toHash );
+
+    // thanx to alexd for this! (https://stackoverflow.com/users/3246555/alexd)
+    // https://stackoverflow.com/questions/38043954/generate-unique-hash-code-based-on-string
+    public static string CalculateHash(
+        bool caseInsensitive,
+        bool ignoreWhiteSpace,
+        params string?[] toHash
+    )
+    {
+        var sb = new StringBuilder();
+
+        foreach( var item in toHash.Where(x=>!string.IsNullOrEmpty(x)  ) )
+        {
+            sb.Append( ignoreWhiteSpace 
+                           ? caseInsensitive 
+                               ? item!.RemoveWhiteSpace().ToLower() 
+                               : item!.RemoveWhiteSpace()
+                           : caseInsensitive
+                               ? item!.ToLower()
+                               : item! );
+        }
+
+        var shaHash = SHA256.Create();
+
+        var retVal = new StringBuilder();
+
+        // Convert the input string to a byte array and compute the hash.
+        byte[] data = shaHash.ComputeHash( Encoding.UTF8.GetBytes( sb.ToString() ) );
+
+        // Loop through each byte of the hashed data and format each one as a hexadecimal string.
+        foreach( var curChar in data )
+        {
+            retVal.Append( curChar.ToString( "x2" ) );
+        }
+
+        return retVal.ToString();
+    }
+
+    // thanx to Henk Meulekamp for this
+    // https://stackoverflow.com/questions/6219454/efficient-way-to-remove-all-whitespace-from-string
+    public static string RemoveWhiteSpace( this string text ) =>
+        new string( text.Where( c => !char.IsWhiteSpace( c ) )
+                        .ToArray() );
+}
